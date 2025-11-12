ea09aaef6fb900923adf8d9aac20db53c60d39b8
ff-hyunbin <gusdol3767@gmail.com>
2025-11-12 10:48:18 +0900

    11.12

--- chapter07/Assets/2.Scripts/BamsongiGenerator.cs
+++ chapter07/Assets/2.Scripts/BamsongiGenerator.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class BamsongiGenerator : MonoBehaviour
 {
     public GameObject bamsongiPrefab;
+    public float throwForce = 5f;
+    float startValue;
+    float power;
 
     // Update is called once per frame
     void Update()
     {
-        GameObject go = Instantiate(bamsongiPrefab);
-        go.transform.position = new Vector3(transform.position.x,
-            transform.position.y + 1,
-            transform.position.z + 1);
-        go.GetComponent<BamsongiController>().Shoot(new Vector3)(0, 500, 2000);
+        if (Input.GetMouseButtonDown(0))
+        {
+            startValue = Input.mousePosition.y;
+        }
+        if (Input.GetMouseButtonDown(0)) {
+
+            power = Input.mousePosition.y - startValue;
+
+            GameObject go = Instantiate(bamsongiPrefab);
+            go.transform.position = new Vector3(transform.position.x,
+                                                transform.position.y + 1,
+                                                transform.position.z + 1);
+            go.GetComponent<BamsongiController>().Shoot(
+                (transform.forward + transform.up * 0.5f) * throwForce * power
+                );
+        }
     }
 }
